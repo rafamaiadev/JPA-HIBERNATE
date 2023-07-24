cfbc03220a2340f000451e04f45b6f444ff05922
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-07-24 19:41:22 -0300

    dependencies settings

---
+++ projetoJPA/src/main/java/basic/test/NewUser.java
@@ -0,0 +1,23 @@
+package basic.test;
+
+import basic.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class NewUser {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("projetoJPA");
+        EntityManager em = emf.createEntityManager();
+
+        User newuser = new User("diego", "diego@hotmail.com");
+
+        em.getTransaction().begin();
+        em.persist(newuser);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
